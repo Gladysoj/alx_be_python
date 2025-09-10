19ab32aa57b634a100fec63f1e895adedc4d2513
gladysuchola <ucholagladys@gmail.com>
2025-09-10 12:59:38 +0400

    Python Introduction

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+addition_result = number1 + number2
+subtraction_result = number1 - number2
+multiplication_result = number1 * number2
+
+print("The addition of", number1, "and", number2, "is", addition_result)
+print("The subtraction of", number1, "and", number2, "is", subtraction_result)
+print("The multiplication of", number1, "and", number2, "is", multiplication_result)
