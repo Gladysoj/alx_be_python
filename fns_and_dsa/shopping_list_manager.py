a58bab9acc2a7b6ac740b71b08231cb751d6a2ce
gladysuchola <ucholagladys@gmail.com>
2025-09-25 23:33:38 +0400

    shopping list

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,5 @@
 def display_menu():
-    print("\nShopping List Manager")
+    print("Shopping List Manager")
     print("1. Add Item")
     print("2. Remove Item")
     print("3. View List")
@@ -13,31 +13,13 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item =input("Enter the item to add:")
-            shopping_list.append(item)
-            print(f"'{item}' has been added to your shopping list.")
             pass
-
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter the item to remove: ")
-            if item in shopping_list:
-                shopping_list.remove(item)
-                print(f"'{item}' has been removed from your shopping list.")
-            else: 
-                print(f"'{item}' was not found in your shopping list.")
             pass
-
         elif choice == '3':
             # Display the shopping list
-            if shopping_list:
-                print("\nYour Shopping List:")
-                for i, item in enumerate(shopping_list, start=1):
-                    print(f"{i}. {item}")
-                else:
-                    print("Your shopping list is empty.")
             pass
-        
         elif choice == '4':
             print("Goodbye!")
             break
