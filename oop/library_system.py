8cd8a05b817781f6181d3a8080a40d5180bfae63
gladysuchola <ucholagladys@gmail.com>
2025-10-09 15:10:55 +0400

    Library

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,31 +5,44 @@ class Book:
 
     def get_details(self):
         return f"Book: {self.title} by {self.author}"
-    
-    class EBook(Book):
-        def __init__(self, title, author, file_size):
-            super().__init__(title, author)
-            self.file_size = file_size
 
-        def get_details(self):
-            return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+    def __str__(self):
+        return self.get_details()
 
-    class PrintBook(Book):
-        def __init__(self, title, author, page_count):
-            super().__init__(title, author)
-            self.page_count = page_count
 
-        def get_details(self):
-            return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
 
-    class Library:
-        def __init__(self):
-         self.books = []
+    def get_details(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
-        def add_book(self, book):
-            self.books.append(book)
+    def __str__(self):
+        return self.get_details()
 
-            def list_books(self):
-                for book in self.books:
-                    print(book.get_details())                 
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)
+        self.page_count = page_count
+
+    def get_details(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+    def __str__(self):
+        return self.get_details()
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            print(book)  
+                 
     
